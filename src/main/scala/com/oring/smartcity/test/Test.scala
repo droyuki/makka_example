358b0c124c639f5ed37195d3b17b92d1c7b80c2a
Wilson Liao <droyuki@gmail.com>
2016-07-27 14:01:37 +0800

    fix example

--- src/main/scala/com/oring/smartcity/test/Test.scala
+++ src/main/scala/com/oring/smartcity/test/Test.scala
@@ -1,6 +1,6 @@
 package com.oring.smartcity.test
 
-import com.oring.smartcity.microservice.MSController
+import com.oring.smartcity.makka.MSController
 
 /**
   * Created by WeiChen on 2016/6/19.
@@ -8,7 +8,7 @@ import com.oring.smartcity.microservice.MSController
 
 object Test {
   def main(args: Array[String]) {
-    val msc = new MSController(args(0))
+    val msc = new MSController()
     msc.run()
   }
 }
