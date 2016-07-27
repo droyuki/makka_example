aba6865241d04d865637efa144d86c3bc09583dd
Wilson Liao <droyuki@gmail.com>
2016-07-27 18:34:58 +0800

    add eventListen config.

--- src/main/scala/util/MqttJob.scala
+++ src/main/scala/util/MqttJob.scala
@@ -15,41 +15,46 @@ class MqttJob extends Job {
   var mqttClient: MqttClient = _
 
   override def init(): Unit = {
-    val in = new FileInputStream(new File("src/main/resources/mqttConf.json"))
-    val rawJson = Json.parse(in)
-    val topics = (rawJson \ "topic").as[List[String]]
-    val mqttBroker = (rawJson \ "broker").as[String]
-    println(mqttBroker)
-    println(topics)
-    val options = new MqttConnectOptions
-    val sslContext = new SslUtil().getSslContext("mytest", "src/main/resources/cacerts")
-    options.setSocketFactory(sslContext.getSocketFactory)
-    options.setUserName("test")
-    options.setPassword("test".toCharArray())
+    try {
+      val in = new FileInputStream(new File("src/main/resources/mqttConf.json"))
+      val rawJson = Json.parse(in)
+      val topics = (rawJson \ "topics").as[List[String]]
+      val mqttBroker = (rawJson \ "broker").as[String]
+      println(mqttBroker)
+      println(topics)
+      val options = new MqttConnectOptions
+      val sslContext = new SslUtil().getSslContext("mytest", "src/main/resources/cacerts")
+      options.setSocketFactory(sslContext.getSocketFactory)
+      options.setUserName("test")
+      options.setPassword("test".toCharArray())
 
-    mqttClient = new MqttClient(mqttBroker, MqttClient.generateClientId(), new MemoryPersistence)
-    mqttClient.connect(options)
+      mqttClient = new MqttClient(mqttBroker, MqttClient.generateClientId(), new MemoryPersistence)
+      mqttClient.connect(options)
+      mqttClient.subscribe(topics.toArray)
+      val callback = new MqttCallback {
+        override def messageArrived(topic: String, message: MqttMessage): Unit = {
+          topic match {
+            case "/fund/portfolio/req" => pipe("PortfolioJob", new Data(message.toString))
+            case "/fund/tsp/req" => {
+              log.info(message.toString)
+              pipe("Tspjob", new Data(message.toString))
+            }
+            case "/fund/4433/req" => pipe("FfttJob", new Data(message.toString))
+            case "/fund/saveportfolio/req" => pipe("SaveJob", new Data(message.toString))
+          }
+        }
 
-    mqttClient.connect()
-    mqttClient.subscribe(topics.toArray)
-    val callback = new MqttCallback {
-      override def messageArrived(topic: String, message: MqttMessage): Unit = {
-        topic match {
-          case "/fund/portfolio/req" => pipe("PortfolioJob", new Data(message.toString))
-          case "/fund/tsp/req" => pipe("Tspjob", new Data(message.toString))
-          case "/fund/4433/req" => pipe("FfttJob", new Data(message.toString))
-          case "/fund/saveportfolio/req" => pipe("SaveJob", new Data(message.toString))
+        override def connectionLost(cause: Throwable): Unit = {
+          log.error(cause.toString)
+        }
+
+        override def deliveryComplete(token: IMqttDeliveryToken): Unit = {
         }
       }
-
-      override def connectionLost(cause: Throwable): Unit = {
-        log.warning(cause.toString)
-      }
-
-      override def deliveryComplete(token: IMqttDeliveryToken): Unit = {
-      }
+      mqttClient.setCallback(callback)
+    } catch {
+      case e => log.error(e.toString)
     }
-    mqttClient.setCallback(callback)
   }
 
   override def receiveData(data: String): Unit = {
